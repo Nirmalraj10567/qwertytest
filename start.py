23370737b4f3af366657080908323a795777cd63
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-20 10:50:54 +0530

    Update start.py

--- start.py
+++ start.py
@@ -108,28 +108,22 @@ async def handler(event):
 
 @client.on(events.NewMessage(pattern='(?i)https://www.zee5.com'))
 
-#async def handler(event):
 async def handler(event):
 
-    link =event.text.split('/')[-1]
-    link1 =event.text.split('-')[-1]
-    
-    print(link1)
-chat = await event.get_chat()
-url = '''https://zee5-player.vercel.app/player?id={link'''
-#open and read page
-    page = requests.get(url)
+    link =url.split('/')[-1]
+    print(link)
+    chat = await event.get_chat()
+    urlq= f'''https://zee5-player.vercel.app/player?id={link}'''
+    page = requests.get(urlq)
     v = page.text
-#html = v.read()
-#create BeautifulSoup parse-able "soup"
     soup = BS(page.text)
     video = soup.find("video")
-#get the src attribute from the video tag
     SRC = video.find("source").get("src")
+    
+    
     markup = client.build_reply_markup(Button.url("Zee5_Stream",SRC))
-    await client.send_message(chat, SRC)
-
-
+    await client.send_message(chat, "Support @SerialCoIn & @urlicupload\n\n",file=video["poster"], buttons=markup)   
+    #print (SRC)
 @client.on(events.NewMessage(pattern='(?i)https://www.mxplayer.in/show/'))
 
 async def handler(event):
